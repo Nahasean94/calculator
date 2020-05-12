083bdc7edf79ced80cb88973bd2b36436bcd4c9e
Sean <nahaseannjenga@gmail.com>
2020-05-12 19:46:37 +0300

    Added more code analysis stage to pipeline

--- src/main/java/com/example/calculator/services/Calculator.java
+++ src/main/java/com/example/calculator/services/Calculator.java
@@ -3,6 +3,9 @@ package com.example.calculator.services;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class
+ */
 @Service
 public class Calculator {
     public int sum(int a, int b){
